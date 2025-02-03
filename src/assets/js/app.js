482c35a6dc2f90e18616576d75f0168a77be951f
KadaverHR <hasslicheratte@yandex.ru>
2025-02-03 16:57:50 +0300

    1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
   function initSlider() {
     if ($(window).width() < 992) {
       console.log($(window).width());
-      
+
       // Инициализация слайдера, если ширина экрана меньше 992px
       $('.news-carousel').slick({
         slidesToShow: 2,
@@ -134,6 +134,47 @@ $(document).ready(function () {
 
 
   AOS.init();
+
+
+
+
+  ///map
+
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+      center: [53.271363, 36.548694],
+      zoom: 14
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        // hintContent: 'Собственный значок метки',
+        // balloonContent: 'Это красивая метка'
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: 'assets/img/map2.svg',
+        // Размеры метки.
+        iconImageSize: [30, 42],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38]
+      })
+
+
+
+    myMap.geoObjects.add(myPlacemark)
+
+  });
+
 });
 
 
